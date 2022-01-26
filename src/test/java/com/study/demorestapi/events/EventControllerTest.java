3af047d1a713b94ccea0f785bf7c85d347d6b504
nara113 <0554my@naver.com>
2022-01-27 06:44:17 +0900

    입력값 이외에 에러발생

--- src/test/java/com/study/demorestapi/events/EventControllerTest.java
+++ src/test/java/com/study/demorestapi/events/EventControllerTest.java
@@ -2,6 +2,7 @@ package com.study.demorestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,34 @@ public class EventControllerTest {
 
     @Test
     void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("spring")
+                .beginEnrollmentDateTime(LocalDateTime.now())
+                .closeEnrollmentDateTime(LocalDateTime.now())
+                .location("korea")
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(mapper.writeValueAsString(event)))
+            .andDo(print())
+            .andExpect(jsonPath("id").exists())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("id").value(Matchers.not(200)))
+            .andExpect(jsonPath("free").value(Matchers.not(true)))
+            .andExpect(jsonPath("offline").value(Matchers.not(true)));
+    }
+
+    @DisplayName("bad request")
+    @Test
+    void createEvent_fail() throws Exception {
         Event event = Event.builder()
                 .id(200)
                 .name("Spring")
@@ -51,13 +80,7 @@ public class EventControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON)
                 .content(mapper.writeValueAsString(event)))
-            .andDo(print())
-            .andExpect(jsonPath("id").exists())
-            .andExpect(header().exists(HttpHeaders.LOCATION))
-            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-            .andExpect(status().isCreated())
-            .andExpect(jsonPath("id").value(Matchers.not(200)))
-            .andExpect(jsonPath("free").value(Matchers.not(true)))
-            .andExpect(jsonPath("offline").value(Matchers.not(true)));
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
